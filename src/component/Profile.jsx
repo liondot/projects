c6075904e999f8e08ecf0b9294ab34486fd1d20f
yoon <funfunjulia@gmail.com>
2023-03-06 11:07:06 +0900

    first commit

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import '../style/profile.scss'
-import profile from '../img/myprofile.png'
+// import profile from '../../public/myprofile.png'
 
 
 
@@ -10,13 +10,13 @@ function Profile() {
     <div className="profile">
         <Link to='./'>
         <div className="my_image">
-            <img src={profile} alt='프로필' />
+          <img src="./image/myprofile.png" alt="" />
         </div>
         
         <div className="desc">
             <span className='myName'>윤선화</span>
             <span className='myJob'>프론트엔드</span>
-            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, nemo.</p> */}
+            
         </div>
         </Link>
     </div>
